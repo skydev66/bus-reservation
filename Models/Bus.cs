c1283f840b4234ee86f04ec2dc4b31f56bd5951d
skydev66 <kin.morita@yandex.com>
2020-03-11 08:31:21 +0800

    bus reservation

--- Models/Bus.cs
+++ Models/Bus.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
+//Online Bus reservation
 namespace OnlineBusReservationV6.Models
 {
     public class Bus
--- Models/BusDriverView.cs
+++ Models/BusDriverView.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+//Model Define for bus reservation
 namespace OnlineBusReservationV6.Models
 {
     public class BusDriverView
